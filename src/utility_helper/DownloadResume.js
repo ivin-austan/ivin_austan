321e80381de4b39888c28d369bee59f2d29945d3
ivin-austan <a.ivinaustan@gmail.com>
2026-08-23 11:07:37 +0400

    fix: updated resume to match ats score of 89

--- src/utility_helper/DownloadResume.js
+++ src/utility_helper/DownloadResume.js
@@ -1,12 +1,11 @@
-import myresume from "../doc/Ivin's_resume.pdf";
+import myresume from "../doc/Ivin's_Resume.pdf";
 
-
-export const DownloadResume=()=> {
-    const filePath = myresume; // Path to your resume file in the folder
-    const link = document.createElement("a"); // Create an anchor element
-    link.href = filePath;
-    link.download = `Ivin Austan's resume`; // Specify the file name for download
-    document.body.appendChild(link); // Append the anchor element to the body
-    link.click(); // Trigger the click event on the anchor element
-    document.body.removeChild(link); // Remove the anchor element from the body
-}
+export const DownloadResume = () => {
+  const filePath = myresume; // Path to your resume file in the folder
+  const link = document.createElement("a"); // Create an anchor element
+  link.href = filePath;
+  link.download = `Ivin Austan's resume`; // Specify the file name for download
+  document.body.appendChild(link); // Append the anchor element to the body
+  link.click(); // Trigger the click event on the anchor element
+  document.body.removeChild(link); // Remove the anchor element from the body
+};
