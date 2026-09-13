6825c7df154d56fabb71aee4c9df090e5ba49f96
ivin-austan <a.ivinaustan@gmail.com>
2026-09-13 18:54:42 +0400

    fix: resume downloading correctly

--- src/utility_helper/DownloadResume.js
+++ src/utility_helper/DownloadResume.js
@@ -4,7 +4,7 @@ export const DownloadResume = () => {
   const filePath = myresume; // Path to your resume file in the folder
   const link = document.createElement("a"); // Create an anchor element
   link.href = filePath;
-  link.download = `Ivin Austan's resume`; // Specify the file name for download
+  link.download = `Ivin's_Resume`; // Specify the file name for download
   document.body.appendChild(link); // Append the anchor element to the body
   link.click(); // Trigger the click event on the anchor element
   document.body.removeChild(link); // Remove the anchor element from the body
